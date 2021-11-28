687d686e3f12401fc55b2c301c33dfab2bd9badc
jkwon <kwonejk@gmail.com>
2021-11-28 23:12:23 +0800

    Login-Getting Username

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector('#login-form'); 
 const loginInput = document.querySelector("#login-form input"); 
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME ="hidden"
 
 function onLoginSubmit(event) {
     event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);   
     const username = loginInput.value;
-    console.log(username);    
+    greeting.innerText = `Hellow ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
